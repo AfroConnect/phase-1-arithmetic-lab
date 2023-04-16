0a0c2487d8a59ccea161f43fbbeeaf59b3c71d5b
AfroConnect <mensahmedia23@gmail.com>
2023-04-16 02:21:55 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 18;
+const num4 = 7;
+const mod = num3 % num4;
+
+const set = [2, 4, 6, 8, 10, 20, 18];
+const max = Math.max(...set);
+
